41e461140844a7413c0c70441c3e463e8724816e
Kai Davenport <kaiyadavenport@gmail.com>
2014-03-15 23:55:34 +0000

    nice crud editor

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ var templates = {
 angular
   .module(modulename, [
     require('digger-editor'),
-    require('file-uploader'),
-    require('./crud')
+    require('file-uploader')
   ])
 
   .factory('$pathSelector', function() {
@@ -26,6 +25,98 @@ angular
     }
 
   })
+
+
+  .directive('crudEditor', function($safeApply, $containerTreeData, $getAncestors, $pathSelector){
+
+    //field.required && showvalidate && containerForm[field.name].$invalid
+    return {
+      restrict:'EA',
+      scope:{
+        container:'=',
+        settings:'='
+      },
+      replace:true,
+      transclude:true,
+      template:templates.crudeditor,
+      controller:function($scope){
+
+        $scope.showform = false;
+
+        $scope.selector = {
+          limit:'0,100',
+          search:''
+        }
+
+        $scope.edit_container = $scope.container;
+        $scope.mode = 'overview';
+
+        var load_trigger = null;
+
+        $scope.run_selector = function(){
+          var selector = '> *' + ($scope.selector.limit ? ':limit(' + $scope.selector.limit + ')' : '');// + ($scope.selector.sort ? ':sort(' + $scope.selector.sort + ')' : '');
+          
+          $scope.edit_container(selector).ship(function(children){
+            
+            $safeApply($scope, function(){
+
+              $scope.folders = children.find('folder').containers();
+              $scope.items = children.find('folder:not').containers();
+
+            })
+          })
+        }
+
+        $scope.$watch('selector', function(selector){
+          $scope.run_selector();
+        }, true)
+
+        // load the tree data
+        $scope.$watch('container', function(container){
+          if(!container){
+            return container;
+          }
+          container('> *:tree(folder)').ship(function(tree){
+            $safeApply($scope, function(){
+              container.get(0)._children = tree.models;
+              $scope.tree_root = container;
+              $scope.$emit('crud:tree:loaded');
+            })
+          })
+        })
+
+
+        $scope.$on('tree:selected', function($e, container){
+          
+          $scope.edit_container = container;
+          $scope.run_selector();
+          
+        })
+
+        $scope.$on('editor:mode', function($ev, mode){
+          $scope.mode = mode;
+        })
+
+      },
+      link:function($scope, $elem, $attrs){
+        
+        // back to top
+        setTimeout(function () {
+
+          $('#treeholder').affix({
+            offset: {
+              top: 0
+            , bottom: function () {
+                return (this.bottom = $('#footer').outerHeight(true))
+              }
+            }
+          })
+            
+        }, 100)
+      }
+    }
+  })
+
 
 
   .directive('folderEditor', function($safeApply){
@@ -76,138 +167,12 @@ angular
     }
   })
 
-
-
-  .directive('crudEditor', function($safeApply, $containerTreeData, $getAncestors, $pathSelector, $crudController){
-
-    //field.required && showvalidate && containerForm[field.name].$invalid
-    return {
-      restrict:'EA',
-      scope:{
-        container:'=',
-        settings:'=',
-        blueprint:'='
-      },
-      replace:true,
-      transclude:true,
-      template:templates.crudeditor,
-      controller:function($scope){
-
-        // load the tree data
-        $scope.$watch('container', function(container){
-          if(!container){
-            return container;
-          }
-
-          console.log('-------------------------------------------');
-          console.log('loading folder tree');
-          container('> *:tree(folder)').ship(function(){
-            console.log('-------------------------------------------');
-            console.dir(container.toJSON());
-            $safeApply($scope, function(){
-              $scope.$emit('crud:tree:loaded');
-              $scope.tree_root = container;
-            })
-          })
-        })
-
-        console.log('-------------------------------------------');
-        console.dir('blueprint');
-        console.dir($scope.blueprint.toJSON());
-        
-        
-
-        //$crudController($scope, $scope.settings);
-          
-
-
-
-/*
-
-        $scope.$on('editing', function(){
-          $scope.editing = true;
-        })
-
-        $scope.$on('notediting', function(){
-          $scope.editing = false;
-        })
-
-        $scope.$on('tree:selected', function($e, container){
-          
-          var ancestors = $getAncestors($scope.resources_root, container) || [];
-
-          // create the url from the #ids mapped into a path
-          var url = [''].concat(ancestors.map(function(ancestor){
-            return ancestor.id();
-          })).concat([container.id()]).join('/');
-
-          $location.path(url);
-
-          $scope.cancelform();
-          
-        })
-
-        // this means the container pointed to via the URL has been loaded
-        $scope.$on('activecontainer_loaded', function($e, container, ancestors){
-
-          $containerTreeData.load($scope, $scope.selectorbase).then(function(tree){
-            $scope.resources_root = tree;
-
-            setTimeout(function(){
-              $safeApply($scope, function(){
-                
-                var resources_seen = false;
-                ancestors.each(function(ancestor){
-                  if(!resources_seen){
-                    if(ancestor.id()=='resources'){
-                      resources_seen = true;
-                    }
-                    return;
-                  }
-                  $scope.$broadcast('tree:expand', ancestor);
-                })
-
-              })
-            }, 200)
-            
-            $scope.$broadcast('tree:select', container, ancestors);
-            
-          })
-      
-          
-
-
-        })
-
-        */
-      
-      },
-      link:function($scope, $elem, $attrs){
-        
-        // back to top
-        setTimeout(function () {
-
-          $('#treeholder').affix({
-            offset: {
-              top: 0
-            , bottom: function () {
-                return (this.bottom = $('#footer').outerHeight(true))
-              }
-            }
-          })
-            
-        }, 100)
-      }
-    }
-  })
-
-  .directive('simpleEditor', function(){
+  .directive('simpleEditor', function($growl, $safeApply){
 
     return {
       restrict:'EA',
       scope:{
         container:'=',
-        blueprint:'=',
         settings:'='
       },
       replace:true,
@@ -215,20 +180,27 @@ angular
       template:templates.simpleeditor,
       controller:function($scope){
 
-        $scope.blueprintname = $scope.blueprint.title();
-        $scope.itemtype = $scope.container.tag();
-        $scope.showadd = true;//$scope.add_mode;
-        $scope.showform = false;
-        $scope.showdelete = false;
+        $scope.settings = $scope.settings || {};
         $scope.folderblueprint = $digger.blueprint.get('folder');
 
-        $scope.options = $scope.options || {};
+        $scope.updatecontainer = function(){
+          $scope.itemtype = $scope.container.tag();
+          $scope.edit_mode = $scope.container.tag()!='_supplychain';
+          $scope.addbuttons = $scope.settings.blueprintfn ? $scope.settings.blueprintfn($scope.container) : ['folder'];
+          $scope.$emit('editor:update', $scope.container);
+        }
 
-        $scope.addclicked = function(){
-          $scope.showadd = false;
-          $scope.showdelete = false;
+        $scope.$watch('container', function(container){
+          if(!container){
+            return;
+          }
+          $scope.updatecontainer();
+        })
 
-          $scope.edit_container = $digger.blueprint.create($scope.blueprintname);
+        $scope.addclicked = function(blueprintname){
+          $scope.blueprintname = blueprintname;
+          $scope.edit_container = $digger.blueprint.create(blueprintname || 'folder');
+          $scope.blueprint = $digger.blueprint.get(blueprintname || 'folder');
 
           $scope.formtitle = 'New ' + ($scope.blueprintname.replace(/^./, function(st){
             return (st || '').toUpperCase();
@@ -237,81 +209,14 @@ angular
           $scope.formactiontitle = 'Add';
           $scope.showform = true;
           $scope.addingmode = true;
+          $scope.editing = true;
 
           $scope.$emit('editor:new', $scope.edit_container);
           $scope.$emit('editing');
         }
-
-        $scope.addfolderclicked = function(){
-          
-          $scope.showadd = false;
-          $scope.showdelete = false;
-          $scope.addingmode = false;
-
-          $scope.edit_folder = $digger.create('folder');
-
-          $scope.formtitle = 'Add Folder';
-
-          $scope.formactiontitle = 'Add';
-
-          $scope.showfolderform = true;
-          $scope.$emit('editing');
-        }
-
-        $scope.editfolderclicked = function(){
-          
-          $scope.showadd = false;
-          $scope.showdelete = false;
-          $scope.addingmode = false;
-
-          $scope.edit_folder = $digger.create('folder');
-
-          $scope.formtitle = 'Add Folder';
-
-          $scope.formactiontitle = 'Add';
-
-          $scope.showfolderform = true;
-          $scope.$emit('editing');
-        }
-
-
-        $scope.cancelfolderform = function(){
-          $scope.cancelform();
-          $scope.$emit('notediting');
-        }
-
-
-        $scope.submitfolderform = function(){
-
-          if($scope.formactiontitle=='Add'){
-            var addfolder = $scope.edit_folder;
-            addfolder.id(addfolder.attr('name').replace(/\s+/g, '_').replace(/\W/g, '').toLowerCase());
-            $scope.container.append(addfolder).ship(function(){
-              $scope.$emit('editor:growl', addfolder.title() + ' added');
-              //load_containers();
-              $safeApply($scope, function(){
-                $scope.cancelform();
-              })
-            })
-          }
-          else{
-            $scope.edit_folder.save().ship(function(){
-              $scope.$emit('editor:growl', $scope.edit_folder.title() + " saved");
-              if(options.foldersaved){
-                options.foldersaved($scope.edit_folder);
-              }
-              //load_containers();
-              $safeApply($scope, function(){
-                $scope.cancelform();
-              })
-            });
-          }
-
-          
-        }
-
-
+        
         $scope.editrow = function(row){
+          $scope.clickmode = 'edit';
           $scope.showadd = false;
           $scope.showdelete = false;
 
@@ -320,6 +225,7 @@ angular
           $scope.edit_container = row;
           $scope.showform = true;
           $scope.addingmode = false;
+          $scope.editing = true;
 
           $scope.canceldata = JSON.parse(JSON.stringify(row.models))
 
@@ -329,10 +235,6 @@ angular
 
         $scope.deleterow = function(row, event){
           $scope.formtitle = 'Delete?';
-          $scope.showdelete = true;
-          $scope.showform = false;
-          $scope.showadd = false;
-          $scope.addingmode = false;
           $scope.edit_container = row;
         }
 
@@ -342,16 +244,19 @@ angular
           $scope.addingmode = false;
           $scope.showfolderform = false;
           $scope.showdelete = false;
+          $scope.editing = false;
           $scope.showadd = $scope.add_mode;
           if($scope.canceldata){
             $scope.edit_container.models = $scope.canceldata;  
           }
           $scope.canceldata = null;
 
+/*
           if($scope.edit_container){
             $location.hash($scope.edit_container.diggerid());
             $anchorScroll();
           }
+*/
           
           $scope.edit_container = null;
           $scope.$emit('notediting');
@@ -362,15 +267,16 @@ angular
           
           $scope.showform = false;
           $scope.showdelete = false;
+          $scope.editing = false;
           $scope.showadd = $scope.add_mode;
 
           var removing = $scope.edit_container;
 
           $scope.edit_container = null;
           removing.remove().ship(function(){
-            $scope.$emit('editor:growl', title + " removed");
+            $growl(title + " removed");
             $scope.$emit('notediting');
-            $scope.$emit('container:delete', removing);
+            $scope.$emit('editor:delete', removing);
             //load_containers();
             
           })
@@ -381,12 +287,11 @@ angular
           function normal_add(newcontainer){
 
             $scope.container.append(newcontainer).ship(function(){
-              $scope.$emit('editor:growl', newcontainer.title() + " added");
+              $growl(newcontainer.title() + " added");
               
-              $scope.$emit('container:add', newcontainer);
-              load_containers();
-              if(options.post_append){
-                options.post_append(newcontainer);
+              $scope.$emit('editor:add', newcontainer);
+              if($scope.settings.post_append){
+                $scope.settings.post_append(newcontainer);
               }
               else{
                 
@@ -401,10 +306,9 @@ angular
           function normal_save(savecontainer){
 
             savecontainer.save().ship(function(){
-              $scope.$emit('editor:growl', savecontainer.title() + " saved");
+              $growl(savecontainer.title() + " saved");
 
-              $scope.$emit('container:save', savecontainer);
-              load_containers();
+              $scope.$emit('editor:save', savecontainer);
               $safeApply($scope, function(){
                 $scope.cancelform();
               })
@@ -412,11 +316,11 @@ angular
             
           }
 
-          if($scope.formactiontitle=='Add'){
+          if($scope.clickmode=='add'){
             var newcontainer = $scope.edit_container;
 
-            if($scope.options.process_add){
-              $scope.options.process_add(newcontainer, function(){
+            if($scope.settings.process_add){
+              $scope.settings.process_add(newcontainer, function(){
                 $safeApply($scope, function(){
                   normal_add(newcontainer);
                 })
@@ -430,8 +334,8 @@ angular
           else{
             var savecontainer = $scope.edit_container;
 
-            if($scope.options.process_save){
-              $scope.options.process_save(savecontainer, function(){
+            if($scope.settings.process_save){
+              $scope.settings.process_save(savecontainer, function(){
                 $safeApply($scope, function(){
                   normal_save(savecontainer);
                 })
@@ -454,21 +358,36 @@ angular
     return {
       restrict:'EA',
       scope:{
-        selected:'=',
         rows:'=',
-        showsummary:'@',
-        selectrow:'&',
-        deleterow:'&',
+        iconfn:'=',
         search:'='
       },
       replace:true,
-      transclude:true,
       template:templates.simpletable,
       link:function($scope, elem, $attrs){
 
-        $scope.showsearch = ($attrs.showsearch || '').match(/\w/);
-
-
+        $scope.showsummary = true;
       }
+    }
+  })
+
+  .factory('$growl', function(){
+    return function(message, type){
+      type = type || 'info';
+      //window.scrollTo(0,0);
+
+      var appendto = $('.add-growl');
+
+      var elemselector = appendto.length>0 ? appendto : 'body';
+      $.bootstrapGrowl(message, {
+        ele: elemselector, // which element to append to
+        type: type, // (null, 'info', 'error', 'success')
+        offset: {from: 'top', amount: 20}, // 'top', or 'bottom'
+        align: 'right', // ('left', 'right', or 'center')
+        width: 250, // (integer, or 'auto')
+        delay: 4000,
+        allow_dismiss: true,
+        stackup_spacing: 10 // spacing between consecutively stacked growls.
+      });
     }
   })
